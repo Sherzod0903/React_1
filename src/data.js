f4c739d439efaaca71c7a4b66b8e91e25ed8cfa3
sherzod0903 <sherzod.sh171@gmail.com>
2022-09-18 20:45:15 +0900

    CRUD

---
+++ src/data.js
@@ -0,0 +1,14 @@
+
+
+
+const data =[
+    {id:1,name:'SHerzod',status:'Enterprenuer'},
+    {id:2,name:'Timur',status:'Student'},
+    {id:3,name:'Umar',status:'Worker'},
+    {id:4,name:'SHahzod',status:'Alpinist'},
+    {id:5,name:'AbdulAziz',status:'MArketolog'},
+
+
+]
+
+export default data;
