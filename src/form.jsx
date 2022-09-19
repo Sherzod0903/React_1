b711066e88d71b4a95cf00050eac43242f489d05
sherzod0903 <sherzod.sh171@gmail.com>
2022-09-19 21:52:13 +0900

    Form with React

---
+++ src/form.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Form extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            email:'',
+            password:''
+        }
+
+    }
+render(){
+
+const getForm=(e)=>{
+    const {value,name}=e.target;
+    this.setState({[name] : value})
+}
+
+return(
+    <div className="form">
+       <div className="form2">
+        <h2>SIGN UP</h2>
+        <h3>Given e-mail : {this.state.email}</h3>
+        <input className=" kir" type="email" name='email' placeholder=" Email" onChange={getForm} />
+        <h3>Given password : {this.state.password}</h3>
+        <input className=" kir"  type="password" name='password' placeholder=" Password" onChange={getForm} /><br/>
+        <button  onChange={getForm}> SIGHN UP</button>
+        
+        
+        
+        <div className="chiziq">
+            <hr className="chiz"></hr>
+            <div className="or"><p> Or</p> </div>
+            <hr className="chiz"></hr>
+
+            </div>
+
+
+    </div></div>
+)
+
+}
+
+
+
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
-import './index'
+import './index.css'
+import Form from './form'
 
 
 
@@ -15,7 +16,7 @@ root.render(
   
   <React.StrictMode>
 
-
+<Form/>
 
 </React.StrictMode>
 )
