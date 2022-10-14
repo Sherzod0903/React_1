a47bca0c4b3c934e7d170194827da8d8b8ee7587
sherzod0903 <sherzod.sh171@gmail.com>
2022-10-15 06:54:21 +0900

    add and delete

---
+++ src/mock.js
@@ -0,0 +1,10 @@
+export const student=[
+    {id:'1',name:'Abubakir',statuss:'It'},
+    {id:'2',name:'Ali',statuss:'BA'},
+
+    {id:'3',name:'Umar',statuss:'BS'},
+    {id:'4',name:'Abdurahmon',statuss:'CS'},
+
+    {id:'5',name:'Abubakir',statuss:'AI'}
+
+]
--- src/state.jsx
+++ src/state.jsx
@@ -1,56 +1,156 @@
 import React from "react";
+ import {student} from "./mock"
 
 class Student extends React.Component {
     constructor(props){
     super(props);
     this.state={
         count: 1,
-        title: "Plus minus project",
+        data:student,
+        name: "",
+        statuss: "",
+        search:"id"
     };
     }
 
     render() {
       
 
-        const plus=()=>{
-            if(this.state.count<10)
-            this.setState({count:this.state.count+1});
-        }
-
-    
-    const minus = () => {
-        if (this.state.count > 1)
-        this.setState({count:this.state.count-1});
-    }
 
     const onChange = (event) => {
-        console.log(event.target.value);
-        this.setState({title: event.target.value})
+        console.log(event.target.name);
+        this.setState({[event.target.name]: event.target.value})
 
     }
 
-    const onSelect = (event) =>{
-        console.log(event.target.value);
-        this.setState({})
-    }
-    const onCheck = (e)=>{
-console.log(e.target.checked);
-    }
+  
+
+
+
+
+const onFilter=(e)=>{
+// console.log(e.target.value);
+const {value}=e.target
+let res=student.filter((item)=> item[this.state.search].toLowerCase().includes(value.toLowerCase()));
+this.setState({data:res,});
+}
+
+
+
+
+const onDelete = (id )=>{
+    console.log(id );
+    let  res=this.state.data.filter(item=>item.id !==id)
+    this.setState({data:res});
+   
+}
+
+
+
+const onSelect=e=>{
+  const{value}=e.target;
+  this.setState({search:value})
+}
+
+
+
+
+
+
+const onAdd=()=>{
+   let user={
+    id: Date.now(),
+    name: this.state.name,
+    statuss: this.state.statuss
+   }
+   this.setState({
+    data:[...this.state.data,user],
+    name:'',
+    statuss:''
+})
+}
+
+
+
+
+
 return(
 
     <div>
-        <h1>{this.state.title}</h1>
-        <h1>State {this.state.count}</h1>
-        <input type="text" onChange={onChange} placeholder="name" />
-        <select onChange={onSelect} name="" id="">
-            <option value="male">male</option>
-            <option value="female">female</option>
+        <h1> Name : {this.state.name}</h1>
+        <h1>Status : {this.state.statuss}</h1>
+
+      
+
+
+
+
+        <input value={this.state.name}  name='name' type="text" onChange={onChange} placeholder="name" />
+        <input value={this.state.statuss} name='statuss' type="text" onChange={onChange} placeholder="status " />
+       
+<button onClick={onAdd}>
+    Add
+</button>
+
+
+
+
+      
+       
+
+<br/>
+<br/>
+<select onChange={onSelect} name="" id="">
+            <option value="id">Id</option>
+            <option value="name">Name</option>
+            <option value="statuss">Status</option>
 
         </select>
-        <input onChange={onCheck} type="checkbox" />
-        <button onClick={plus}>+</button>
-        <button onClick={minus}>-</button>
+<input onChange={onFilter} type="text" placeholder="filter" />
 
+<br/>
+
+<table border='1' width='100%'>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Delete</th>
+            <th>Edit</th>   
+
+
+        </tr>
+    </thead>
+    <tbody>
+
+    
+
+
+        {
+            this.state.data.length?
+        this.state.data.map(({id,name,statuss})=> {
+            return(
+                <tr key={id}> 
+                <td>{id}</td> 
+                <td>{name} </td>
+                <td> {statuss}</td>
+                <td>
+                    <button onClick={()=>onDelete(id)}>delete</button>
+                </td>
+                <td>
+                    <button>edit</button>
+                </td>
+                </tr>
+                )
+            } ) : (<tr >
+            <th colSpan={5}>
+                <h1>no data</h1>
+                </th>
+            </tr>) }
+        
+            </tbody>
+        </table>
 
     </div>
 )
